6bb516edaf18d8ce52c676bba179e4f4dfc96742
Maharaja0003 <144311063+Maharaja0003@users.noreply.github.com>
2023-09-15 12:06:24 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-# Python program to check if year is a leap year or not
+#Python program to create Bankaccount class
+# with both a deposit() and a withdraw() function
+class Bank_Account:
 
-year = 2024
+  def __init__(self):
+    self.balance = 0
+    print("Hello!!! sree Welcome, your Account no (12341234) ")
 
-# To get year (integer input) from the user
-# year = int(input("Enter a year: "))
+  def deposit(self):
+    amount = float(input("Enter amount to be Deposited: "))
+    self.balance += amount
+    print("\n Amount Deposited:", amount)
 
-# divided by 100 means century year (ending with 00)
-# century year divided by 400 is leap year
-if (year % 400 == 0) and (year % 100 == 0):
-    print("{0} is a leap year".format(year))
+  def withdraw(self):
+    amount = float(input("Enter amount to be Withdrawn: "))
+    if self.balance >= amount:
+      self.balance -= amount
+      print("\n You Withdrew:", amount)
+    else:
+      print("\n Insufficient balance  ")
 
-# not divided by 100 means not a century year
-# year divided by 4 is a leap year
-elif (year % 4 ==0) and (year % 100 != 0):
-    print("{0} is a leap year".format(year))
+  def display(self):
+    print("\n Net Available Balance=", self.balance)
 
-# if not divided by both 400 (century year) and 4 (not century year)
-# year is not leap year
-else:
-    print("{0} is not a leap year".format(year))
+
+# Driver code
+
+# creating an object of class
+s = Bank_Account()
+
+# Calling functions with that class object
+s.deposit()
+s.withdraw()
+s.display()
